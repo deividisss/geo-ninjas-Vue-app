d02f11bfd94c496925c61507d3ea975c1e87ed00
Deividas <deivis124@gmail.com>
2020-07-23 23:36:21 +0300

    Sets upps Firebase

---
+++ src/firebase/init.js
@@ -0,0 +1,17 @@
+import firabse from "firebase";
+import firestore from "firebase/firestore";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBBwj4qAzWYrqbbC3NZreX9j5MGCIv9GxU",
+  authDomain: "geo-nijas.firebaseapp.com",
+  databaseURL: "https://geo-nijas.firebaseio.com",
+  projectId: "geo-nijas",
+  storageBucket: "geo-nijas.appspot.com",
+  messagingSenderId: "381391646222",
+  appId: "1:381391646222:web:b11060f8e59896c9fad65f"
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp.firestore();
