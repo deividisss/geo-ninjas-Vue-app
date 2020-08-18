e135033b02e0dd6e39d6ab95aa82fe9cd0a91999
Deividas <deivis124@gmail.com>
2020-08-18 20:30:14 +0300

    Adds additional styling to user wall

--- src/components/profile/ViewProfile.vue
+++ src/components/profile/ViewProfile.vue
@@ -76,7 +76,7 @@ export default {
         db.collection("comments")
           .add({
             to: this.$route.params.id,
-            from: this.user.id,
+            from: this.user.alias,
             content: this.newCommnet,
             time: Date.now(),
           })
@@ -90,3 +90,20 @@ export default {
   },
 };
 </script>
+
+<style>
+.view-profile .card {
+  padding: 20px;
+  margin-top: 60px;
+}
+
+.view-profile h2 {
+  font-size: 2em;
+  margin-top: 0;
+}
+
+.view-profile li {
+  padding: 10px;
+  border-bottom: 1px solid #eee;
+}
+</style>
